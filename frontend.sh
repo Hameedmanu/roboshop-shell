d5f37c2a9235412d5af1dc503acae7a2fbdcb75a
Hameedmanu <manuhameed1005@gmail.com>
2024-11-29 16:19:12 +0530

    Shell scripts of roboshop update DNS records

--- frontend.sh
+++ frontend.sh
@@ -1,9 +1,7 @@
-cp nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
 dnf install nginx -y
-rm -rf /usr/share/nginx/html/*
-
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
-
+cp nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
+sudo rm -rf /usr/share/nginx/html/*
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
