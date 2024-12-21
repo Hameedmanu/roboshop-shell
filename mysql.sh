3823ba4a6daf62ac3271149758f20e001f9da192
Hameedmanu <manuhameed1005@gmail.com>
2024-12-21 16:29:38 +0530

    Shell scripts of roboshop updated mysql.sh file

--- mysql.sh
+++ mysql.sh
@@ -1,5 +1,7 @@
 cp mysql.repo /etc/yum.repos.d/mysql.repo
+
 dnf module disable mysql -y
 dnf install mysql-community-server -y
+sudo systemctl enable mysql
+sudo systemctl start mysql
 mysql_secure_installation --set-root-pass RoboShop@1
-mysql -uroot -pRoboShop@1
