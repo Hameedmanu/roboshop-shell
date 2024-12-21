a0f0b552ffe984a3b7e574edbc6ade59e2dfbdc6
Hameedmanu <manuhameed1005@gmail.com>
2024-12-21 13:58:22 +0530

    Shell scripts of roboshop updated cart service and cart with sudo

--- cart.sh
+++ cart.sh
@@ -6,7 +6,7 @@ dnf module enable nodejs:18 -y
 dnf install nodejs -y
 useradd roboshop
 mkdir /app
- curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip
+ curl -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip
  cd /app
  unzip /tmp/cart.zip
  cd /app
