e8e88e9253b35f2cb5b9e768263764438d0102c7
Hameedmanu <manuhameed1005@gmail.com>
2024-12-22 18:08:21 +0530

    project Catalouge script - add redirector

--- catalouge.sh
+++ catalouge.sh
@@ -1,49 +1,49 @@
 echo -e "\e[33m>>>>>>>>>>  Create Catalouge Service <<<<<<<<<<\e[0m"
-cp catalouge.service /etc/systemd/system/catalouge.service >/tmp/roboshop.log
+cp catalouge.service /etc/systemd/system/catalouge.service &>/roboshop.log
 
 echo -e "\e[33m>>>>>>>>>>  Create MongoDB Repo <<<<<<<<<<\e[0m"
-cp mongo.repo /etc/yum.repos.d/mongo.repo  >/tmp/roboshop.log
+cp mongo.repo /etc/yum.repos.d/mongo.repo  &>/roboshop.log
 
 echo -e "\e[33m>>>>>>>>>>  Install NodeJS Repos <<<<<<<<<<\e[0m"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash  >/tmp/roboshop.log
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash  &>/roboshop.log
 
 echo -e "\e[33m>>>>>>>>>>  Disable NodeJS <<<<<<<<<<\e[0m"
-dnf module disable nodejs -y  >/tmp/roboshop.log
+dnf module disable nodejs -y  &>/roboshop.log
 
 echo -e "\e[33m>>>>>>>>>>  Enable NodeJS Module<<<<<<<<<<\e[0m"
-dnf module enable nodejs:18 -y  >/tmp/roboshop.log
+dnf module enable nodejs:18 -y  &>/roboshop.log
 
 echo -e "\e[33m>>>>>>>>>>  Install NodeJS <<<<<<<<<<\e[0m"
-dnf install nodejs -y  >/tmp/roboshop.log
+dnf install nodejs -y  &>/roboshop.log
 
 echo -e "\e[33m>>>>>>>>>>  Create Application User <<<<<<<<<<\e[0m"
-useradd roboshop  >/tmp/roboshop.log
+useradd roboshop  &>/roboshop.log
 
 
 echo -e "\e[33m>>>>>>>>>>  Remove Application Directory <<<<<<<<<<\e[0m"
-rm -rf /app  >/tmp/roboshop.log
+rm -rf /app  &>/roboshop.log
 
 echo -e "\e[33m>>>>>>>>>>  Create Application Directory <<<<<<<<<<\e[0m"
-mkdir /app  >/tmp/roboshop.log
+mkdir /app  &>/roboshop.log
 
 echo -e "\e[33m>>>>>>>>>>  Download Application content <<<<<<<<<<\e[0m"
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip  >/tmp/roboshop.log
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip  &>/roboshop.log
 
 echo -e "\e[33m>>>>>>>>>>  Extract Application content <<<<<<<<<<\e[0m"
 cd /app
-unzip /tmp/catalogue.zip  >/tmp/roboshop.log
+unzip /tmp/catalogue.zip  &>/roboshop.log
 cd /app
 
 echo -e "\e[33m>>>>>>>>>>  Download NodeJS Dependencies <<<<<<<<<<\e[0m"
-npm install  >/tmp/roboshop.log
+npm install  &>/roboshop.log
 
 echo -e "\e[33m>>>>>>>>>> Install MongoDB client  <<<<<<<<<<\e[0m"
-dnf install mongodb-org-shell -y  >/tmp/roboshop.log
+dnf install mongodb-org-shell -y  &>/roboshop.log
 
 echo -e "\e[33m>>>>>>>>>>  Load Catalouge Schema <<<<<<<<<<\e[0m"
-mongo --host mongodb.hmtechops.in </app/schema/catalogue.js  >/tmp/roboshop.log
+mongo --host mongodb.hmtechops.in </app/schema/catalogue.js  &>/roboshop.log
 
 echo -e "\e[33m>>>>>>>>>> Start Catalouge Service <<<<<<<<<<\e[0m"
-sudo systemctl daemon-reload  >/tmp/roboshop.log
-sudo systemctl enable catalogue  >/tmp/roboshop.log
-sudo systemctl restart catalogue >/tmp/roboshop.log
+sudo systemctl daemon-reload  &>/roboshop.log
+sudo systemctl enable catalogue  &>/roboshop.log
+sudo systemctl restart catalogue &>/roboshop.log
