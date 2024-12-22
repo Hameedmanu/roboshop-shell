4761dc6850f3726ea055dc25d9c1a70e0a0e61f7
Hameedmanu <manuhameed1005@gmail.com>
2024-12-22 15:57:30 +0530

    roboshop-shell update with echo with Heading

--- catalouge.sh
+++ catalouge.sh
@@ -1,21 +1,45 @@
+echo ">>>>>>>>>>  Create Catalouge Service <<<<<<<<<<<"
 cp catalouge.service /etc/systemd/system/catalouge.service
+
+echo ">>>>>>>>>>  Create MongoDB Repo <<<<<<<<<<<"
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 
+echo ">>>>>>>>>>  Install NodeJS Repos <<<<<<<<<<<"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-dnf module disable nodejs -y
-dnf module enable nodejs:18 -y
-dnf install nodejs -y
-useradd roboshop
-mkdir /app
- curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
- cd /app
- unzip /tmp/catalogue.zip
- cd /app
- npm install
 
+echo ">>>>>>>>>>  Disable NodeJS <<<<<<<<<<<"
+dnf module disable nodejs -y
+
+echo ">>>>>>>>>>  Enable NodeJS Module<<<<<<<<<<<"
+dnf module enable nodejs:18 -y
+
+echo ">>>>>>>>>>  Install NodeJS <<<<<<<<<<<"
+dnf install nodejs -y
+
+echo ">>>>>>>>>>  Create Application User <<<<<<<<<<<"
+useradd roboshop
+
+echo ">>>>>>>>>>  Create Application Directory <<<<<<<<<<<"
+mkdir /app
+
+echo ">>>>>>>>>>  Download Application content <<<<<<<<<<<"
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+
+echo ">>>>>>>>>>  Extract Application content <<<<<<<<<<<"
+cd /app
+unzip /tmp/catalogue.zip
+cd /app
+
+echo ">>>>>>>>>>  Download NodeJS Dependencies <<<<<<<<<<<"
+npm install
+
+echo ">>>>>>>>>> Install MongoDB client  <<<<<<<<<<<"
 dnf install mongodb-org-shell -y
+
+echo ">>>>>>>>>>  Load Catalouge Schema <<<<<<<<<<<"
 mongo --host mongodb.hmtechops.in </app/schema/catalogue.js
 
+echo ">>>>>>>>>> Start Catalouge Service <<<<<<<<<<<"
 sudo systemctl daemon-reload
 sudo systemctl enable catalogue
 sudo systemctl restart catalogue
