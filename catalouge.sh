1fed59a978815cdfd6eac6bba0cf470729fa1212
Hameedmanu <manuhameed1005@gmail.com>
2024-12-22 17:15:14 +0530

    Echo command Heading in project Catalouge script

--- catalouge.sh
+++ catalouge.sh
@@ -1,45 +1,45 @@
-echo ">>>>>>>>>>  Create Catalouge Service <<<<<<<<<<<"
+echo -e "\e[33m>>>>>>>>>>  Create Catalouge Service <<<<<<<<<<\e[0m
 cp catalouge.service /etc/systemd/system/catalouge.service
 
-echo ">>>>>>>>>>  Create MongoDB Repo <<<<<<<<<<<"
+echo -e "\e[33m>>>>>>>>>>  Create MongoDB Repo <<<<<<<<<<\e[0m
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 
-echo ">>>>>>>>>>  Install NodeJS Repos <<<<<<<<<<<"
+echo -e "\e[33m>>>>>>>>>>  Install NodeJS Repos <<<<<<<<<<\e[0m
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 
-echo ">>>>>>>>>>  Disable NodeJS <<<<<<<<<<<"
+echo -e "\e[33m>>>>>>>>>>  Disable NodeJS <<<<<<<<<<\e[0m
 dnf module disable nodejs -y
 
-echo ">>>>>>>>>>  Enable NodeJS Module<<<<<<<<<<<"
+echo -e "\e[33m>>>>>>>>>>  Enable NodeJS Module<<<<<<<<<<\e[0m
 dnf module enable nodejs:18 -y
 
-echo ">>>>>>>>>>  Install NodeJS <<<<<<<<<<<"
+echo -e "\e[33m>>>>>>>>>>  Install NodeJS <<<<<<<<<<\e[0m
 dnf install nodejs -y
 
-echo ">>>>>>>>>>  Create Application User <<<<<<<<<<<"
+echo -e "\e[33m>>>>>>>>>>  Create Application User <<<<<<<<<<\e[0m
 useradd roboshop
 
-echo ">>>>>>>>>>  Create Application Directory <<<<<<<<<<<"
+echo -e "\e[33m>>>>>>>>>>  Create Application Directory <<<<<<<<<<\e[0m
 mkdir /app
 
-echo ">>>>>>>>>>  Download Application content <<<<<<<<<<<"
+echo -e "\e[33m>>>>>>>>>>  Download Application content <<<<<<<<<<\e[0m
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
 
-echo ">>>>>>>>>>  Extract Application content <<<<<<<<<<<"
+echo -e "\e[33m>>>>>>>>>>  Extract Application content <<<<<<<<<<\e[0m
 cd /app
 unzip /tmp/catalogue.zip
 cd /app
 
-echo ">>>>>>>>>>  Download NodeJS Dependencies <<<<<<<<<<<"
+echo -e "\e[33m>>>>>>>>>>  Download NodeJS Dependencies <<<<<<<<<<\e[0m
 npm install
 
-echo ">>>>>>>>>> Install MongoDB client  <<<<<<<<<<<"
+echo -e "\e[33m>>>>>>>>>> Install MongoDB client  <<<<<<<<<<\e[0m
 dnf install mongodb-org-shell -y
 
-echo ">>>>>>>>>>  Load Catalouge Schema <<<<<<<<<<<"
+echo -e "\e[33m>>>>>>>>>>  Load Catalouge Schema <<<<<<<<<<\e[0m
 mongo --host mongodb.hmtechops.in </app/schema/catalogue.js
 
-echo ">>>>>>>>>> Start Catalouge Service <<<<<<<<<<<"
+echo -e "\e[33m>>>>>>>>>> Start Catalouge Service <<<<<<<<<<\e[0m
 sudo systemctl daemon-reload
 sudo systemctl enable catalogue
 sudo systemctl restart catalogue
